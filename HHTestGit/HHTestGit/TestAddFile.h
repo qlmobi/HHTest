d80f7dbffc9fdaeb0cc56415104f9089ab7d4122
leo <leoshizj@gmail.com>
2019-01-26 14:03:17 +0800

    addle

---
+++ HHTestGit/HHTestGit/TestAddFile.h
@@ -0,0 +1,17 @@
+//
+//  TestAddFile.h
+//  HHTestGit
+//
+//  Created by Q on 2019/1/26.
+//  Copyright © 2019 Q. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestAddFile : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HHTestGit/HHTestGit/TestAddFile.m
@@ -0,0 +1,13 @@
+//
+//  TestAddFile.m
+//  HHTestGit
+//
+//  Created by Q on 2019/1/26.
+//  Copyright © 2019 Q. All rights reserved.
+//
+
+#import "TestAddFile.h"
+
+@implementation TestAddFile
+
+@end
