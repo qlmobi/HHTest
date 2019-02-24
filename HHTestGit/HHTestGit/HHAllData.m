872706c96de0868b8e85b543b449874f29563c86
shizhijun <zhijun_shi@human-horizons.com>
2019-02-24 14:58:57 +0800

    addHHAlllData

---
+++ HHTestGit/HHTestGit/HHAllData.m
@@ -0,0 +1,13 @@
+//
+//  HHAllData.m
+//  HHTestGit
+//
+//  Created by Q on 2019/2/24.
+//  Copyright © 2019 Q. All rights reserved.
+//
+
+#import "HHAllData.h"
+
+@implementation HHAllData
+
+@end
